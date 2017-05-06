2176c3c98fa4e4dfcf64c36c6f3aa893bdf4387b
Anonymous Maarten <anonymous.maarten@gmail.com>
2017-05-06 10:57:33 +0200

    CMakeAutoGen automatically determines dependences and targets at configuration time

--- CMakeModules/CMakeAutoGen.cmake
+++ CMakeModules/CMakeAutoGen.cmake
@@ -4,19 +4,41 @@
 
 set(AUTOGEN_SCRIPT "${PROJECT_SOURCE_DIR}/CMakeModules/CMakeAutoGenScript.cmake")
 
-function(add_autogen_target INPUT OUTPUTDIR)
-    set(OUTPUTFILES "${ARGN}")
-
+function(autogen_get_depends INPUT OUTPUTDIR _DEPENDS)
     if (OUTPUTDIR)
         set(PREFIX "${OUTPUTDIR}/")
     else()
         set(PREFIX "")
     endif()
 
-    set(ARTIFACTS)
-    foreach(OUTPUTFILE ${OUTPUTFILES})
-        list(APPEND ARTIFACTS "${PREFIX}${OUTPUTFILE}")
-    endforeach()
+    execute_process(
+        COMMAND "${CMAKE_COMMAND}" "-DACTION=DEPENDS" "-DDEFINITION=${CMAKE_CURRENT_SOURCE_DIR}/${INPUT}" -P "${AUTOGEN_SCRIPT}"
+        OUTPUT_VARIABLE AUTOGEN_DEPENDS
+    )
+    string(STRIP "${AUTOGEN_DEPENDS}" AUTOGEN_DEPENDS)
+
+    set("${_DEPENDS}" "${AUTOGEN_DEPENDS}" PARENT_SCOPE)
+endfunction()
+
+function(autogen_get_targets INPUT OUTPUTDIR _TARGETS)
+    if (OUTPUTDIR)
+        set(PREFIX "${OUTPUTDIR}/")
+    else()
+        set(PREFIX "")
+    endif()
+
+    execute_process(
+        COMMAND "${CMAKE_COMMAND}" "-DACTION=TARGETS" "-DDEFINITION=${CMAKE_CURRENT_SOURCE_DIR}/${INPUT}" -P "${AUTOGEN_SCRIPT}"
+        OUTPUT_VARIABLE AUTOGEN_TARGETS
+    )
+    string(STRIP "${AUTOGEN_TARGETS}" AUTOGEN_TARGETS)
+
+    set("${_TARGETS}" "${AUTOGEN_TARGETS}" PARENT_SCOPE)
+endfunction()
+
+function(autogen_add_command INPUT OUTPUTDIR)
+    autogen_get_depends("${INPUT}" "${OUTPUTDIR}" DEPENDS)
+    autogen_get_targets("${INPUT}" "${OUTPUTDIR}" TARGETS)
 
     set(EXTRA_ARGS)
     if (AUTOGEN_DEBUG)
@@ -27,10 +49,10 @@ function(add_autogen_target INPUT OUTPUTDIR)
     endif()
 
     add_custom_command(
-        OUTPUT ${ARTIFACTS}
-        COMMAND ${CMAKE_COMMAND} "-DDEFINITION=${CMAKE_CURRENT_SOURCE_DIR}/${INPUT}" ${EXTRA_ARGS} -P "${AUTOGEN_SCRIPT}"
+        OUTPUT ${TARGETS}
+        COMMAND "${CMAKE_COMMAND}" "-DACTION=TEMPLATE" "-DDEFINITION=${CMAKE_CURRENT_SOURCE_DIR}/${INPUT}" ${EXTRA_ARGS} -P "${AUTOGEN_SCRIPT}"
         MAIN_DEPENDENCY "${INPUT}"
-        DEPENDS "${AUTOGEN_SCRIPT}"
+        DEPENDS ${AUTOGEN_SCRIPT} ${DEPENDS}
         COMMENT "AutoGen: parsing ${INPUT}, generating ${OUTPUTFILES}"
     )
 endfunction()
